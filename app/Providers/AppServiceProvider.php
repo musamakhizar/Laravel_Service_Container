013811b3d8678a9090055c7d287affb27143084b
MUsamaKhizar <usamakhizar07@gmail.com>
2021-08-22 13:28:51 +0500

    Multiple Payment Gateways

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Billing\BankPaymentGateway;
+use App\Billing\CreditPaymentGateway;
 use App\Billing\PaymentGatewayContract;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,7 +17,10 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(PaymentGatewayContract::class,function($app){
-            return new BankPaymentGateway('usd');
+            //return new BankPaymentGateway('usd');
+            return new CreditPaymentGateway('usd');
+
+            //you can dynamicall chose which payment gateway you want to choose
         });
     }
 
