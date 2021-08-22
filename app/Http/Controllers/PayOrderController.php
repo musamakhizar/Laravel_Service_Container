1e5a424c349bced60681cca40be540e5161cf5b2
MUsamaKhizar <usamakhizar07@gmail.com>
2021-08-22 10:54:07 +0500

    Dependency Injection With Argument passing

--- app/Http/Controllers/PayOrderController.php
+++ app/Http/Controllers/PayOrderController.php
@@ -7,9 +7,8 @@ use Illuminate\Http\Request;
 
 class PayOrderController extends Controller
 {
-    public function store()
+    public function store(PaymentGateway $paymentGateway)
     {
-        $paymentGateway = new PaymentGateway('usd');
         dd($paymentGateway->charge(2000));
     }
 }
